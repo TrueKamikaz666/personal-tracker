a008df7a1e02cb3e53e2d8375fb495194461d7fa
TrueKamikaz666 <kamikaz6669@hotmail.com>
2026-07-09 10:56:06 +0200

    Update monsterhigh.py

--- scraper/monsterhigh.py
+++ scraper/monsterhigh.py
@@ -1,6 +1,37 @@
+from bs4 import BeautifulSoup
+
+from utils import download
+from utils import load_json
+
+
+SEARCH_URL = "https://shopping.mattel.com/es-es/search?q={}"
+
+
 def check_monster_high():
 
-    return {
+    config = load_json("../data/config.json")
+
+    result = {
         "found": False,
         "stores": []
     }
+
+    for product in config["monsterHigh"]:
+
+        html = download(
+            SEARCH_URL.format(product["id"])
+        )
+
+        soup = BeautifulSoup(html, "lxml")
+
+        if product["id"] in html.upper():
+
+            result["found"] = True
+
+            result["stores"].append({
+                "store": "Mattel",
+                "product": product["name"],
+                "url": SEARCH_URL.format(product["id"])
+            })
+
+    return result
