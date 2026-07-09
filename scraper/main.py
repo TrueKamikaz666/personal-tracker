1d66ef3915a51890f5f251bb2b930a4aa3d300f5
TrueKamikaz666 <kamikaz6669@hotmail.com>
2026-07-09 11:25:40 +0200

    Update main.py

--- scraper/main.py
+++ scraper/main.py
@@ -1,17 +1,23 @@
-import json
-from datetime import datetime
-
-from monsterhigh import check_monster_high
-from funko import check_funko
+from sources.monsterhigh.mattel import Mattel
 
 
-result = {
-    "lastUpdate": datetime.utcnow().strftime("%Y-%m-%d %H:%M UTC"),
-    "monsterHigh": check_monster_high(),
-    "funko": check_funko()
-}
+sources = [
 
-with open("../docs/data.json", "w", encoding="utf-8") as file:
-    json.dump(result, file, indent=4, ensure_ascii=False)
+    Mattel()
 
-print("data.json actualizado correctamente.")
+]
+
+
+monster_high = []
+
+
+for source in sources:
+
+    monster_high.append(
+
+        source.run()
+
+    )
+
+
+print(monster_high)
