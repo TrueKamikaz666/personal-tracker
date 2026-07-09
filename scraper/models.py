0fbc856179902844c569944530279f65705ef010
TrueKamikaz666 <kamikaz6669@hotmail.com>
2026-07-09 13:48:55 +0200

    Create models.py

---
+++ scraper/models.py
@@ -0,0 +1,21 @@
+def create_product():
+
+    return {
+        "name": "",
+        "price": "",
+        "currency": "EUR",
+        "store": "",
+        "url": "",
+        "image": "",
+        "available": False
+    }def create_product():
+
+    return {
+        "name": "",
+        "price": "",
+        "currency": "EUR",
+        "store": "",
+        "url": "",
+        "image": "",
+        "available": False
+    }
