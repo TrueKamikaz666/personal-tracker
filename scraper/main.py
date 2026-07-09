bad0dcb55aab8c29f6f160eb367dd3c9ee989617
TrueKamikaz666 <kamikaz6669@hotmail.com>
2026-07-09 12:38:49 +0200

    Update main.py

--- scraper/main.py
+++ scraper/main.py
@@ -2,6 +2,9 @@ import json
 from datetime import datetime, UTC
 from pathlib import Path
 
+from amazon_monster_high import scrape_monster_high
+from funko_official import scrape_funko
+
 
 ROOT = Path(__file__).resolve().parent.parent
 
@@ -9,36 +12,24 @@ OUTPUT = ROOT / "docs" / "latest.json"
 
 
 data = {
-
-    "updated": datetime.now(datetime.UTC).isoformat(),
-
-    "monsterHigh": [],
-
-    "funko": []
-
+    "updated": datetime.now(UTC).isoformat(),
+    "monsterHigh": scrape_monster_high(),
+    "funko": scrape_funko()
 }
 
 
 with open(
-
     OUTPUT,
-
     "w",
-
     encoding="utf-8"
-
 ) as file:
 
     json.dump(
-
         data,
-
         file,
-
         indent=4,
-
         ensure_ascii=False
-
     )
+
 
 print("latest.json generated")
